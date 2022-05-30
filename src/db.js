6d3a969dca1c146e431080ddecdd394e96ee7095
fronomenal <94882375+fronomenal@users.noreply.github.com>
2022-05-31 00:46:57 +0100

    Implemented db connection timeout

--- src/db.js
+++ src/db.js
@@ -6,11 +6,27 @@ const conn = mysql.createConnection({
     password : (process.env.DB_PASS)? process.env.DB_PASS : '',
     database : process.env.DB || 'shoppers' 
 });
-conn.connect((err) => {
-    if (err) {
-      throw err;
+
+let poller = setInterval(poll, 3000);
+let flag = true;
+poll(poller);
+setTimeout(() => {
+  clearInterval(poller)
+  setTimeout(()=>{
+    if(flag) throw new Error("Database timeout after 16 seconds"); 
+  }, 500);
+}, 15500);
+
+
+function poll(poller){
+  let errHandler = (errcon)=> {
+    if (!errcon) {
+      console.log("MySql Connected");
+      flag = false;
+      clearInterval(poller);
     }
-console.log("MySql Connected");
-});
+  }
+  conn.connect(errHandler)
+}
 
 module.exports = conn;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const router = require("express").Router();
 const db = require("../db");
 
 router.get("/", (req, res)=>{
-    db.query("SELECT * FROM shoppers.customers;", (err, rez)=>{
+    db.query("SELECT CURDATE();", (err, rez)=>{
         if (err) throw err;
         res.json(rez);
     });
